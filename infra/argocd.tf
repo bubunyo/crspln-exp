e3512e2372448af5ce67d75c083f529ccfafeebf
Bubunyo Nyavor <kidd.bubu@gmail.com>
2026-08-31 15:30:30 +0100

    apply argocd

---
+++ infra/argocd.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "10.4.2"
+  namespace        = "argocd"
+  create_namespace = true
+
+  values = [file("${path.module}/argocd.yaml")]
+
+  depends_on = [google_container_node_pool.primary_nodes]
+}
