a3da50717d1565f7db5e57563a9d7a2f843a2517
Bubunyo Nyavor <kidd.bubu@gmail.com>
2026-09-05 12:30:35 +0100

    add argo app

---
+++ infra/argocd-app.tf
@@ -0,0 +1,31 @@
+resource "kubernetes_manifest" "app" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "app"
+      namespace = "argocd"
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "https://github.com/${var.github_repo}.git"
+        targetRevision = "main"
+        path           = "gitops/overlays/prod"
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "app"
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+        syncOptions = ["CreateNamespace=false"]
+      }
+    }
+  }
+
+  depends_on = [helm_release.argocd, kubernetes_namespace_v1.app]
+}
